bbe87604acfbec2aaeb8f2b466e7c143b3ec8425
usersina <benna.anis2@gmail.com>
2021-11-21 12:19:59 +0100

    Add jwt auth filter

---
+++ server/src/main/java/com/sina/app/security/SecurityParams.java
@@ -0,0 +1,7 @@
+package com.sina.app.security;
+
+public interface SecurityParams {
+    public static final long EXP_TIME = 10 * 24 * 60 * 60 * 1000;
+    public static final String SECRET = "my&super&secret!";
+    public static final String PREFIX = "Bearer ";
+}
