23fc07a1440fc97833ab6bbdce9ed58348fcfd9c
usersina <benna.anis2@gmail.com>
2021-11-21 17:47:13 +0100

    Add security to routes

---
+++ server/src/main/java/com/sina/app/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.sina.app.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // -- NOTE: User entity (api user) IS NOT a LoggedUser entity (security user)
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        // -- Allow access to users' profiles publicly
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/users/**").permitAll();
+
+        // -- Allow only admins & LoggedUsers to get the list of todos
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/todos/**").hasAnyAuthority("ADMIN", "USER");
+
+        // -- Allow adding, editing & deleting for admin only
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/todos/**").hasAnyAuthority("ADMIN");
+        http.authorizeRequests().antMatchers(HttpMethod.PUT, "/todos/**").hasAnyAuthority("ADMIN");
+        http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/todos/**").hasAnyAuthority("ADMIN");
+
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/users/**").hasAnyAuthority("ADMIN");
+        http.authorizeRequests().antMatchers(HttpMethod.PUT, "/users/**").hasAnyAuthority("ADMIN");
+        http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/users/**").hasAnyAuthority("ADMIN");
+
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+}
